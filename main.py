3a5ed8453cbb19f69c5d20aeaac14fdf7ff57c48
d <dol@swit.io>
2024-07-05 14:57:48 +0900

    Add startup trigger

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from werkzeug.serving import is_running_from_reloader
 
 from src.app import create_app
 from src.core.constants import settings
+from src.services.provision_manager import provisioner
 from src.services.scheduler import scheduler
 
 app = create_app()
@@ -10,6 +11,8 @@ if __name__ == "__main__":
     try:
         if not is_running_from_reloader():
             scheduler.initialize()
+            # At start, provisioner will be started
+            provisioner.start()
         app.run(
             host='0.0.0.0',
             port=settings.PORT,
